62cea4086b7631c6a17d10f69f692f7e1b8edf60
karansondh <63238367+karansondh@users.noreply.github.com>
2020-07-22 13:47:18 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -5,15 +5,15 @@ from pathlib import Path
 
 html =Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from']= #enter in ''
-email['to']= #receiver's email in ''
-email['subject']= #enter in ''
+email['from']= 'your.email@gmail.com'
+email['to']= 'reciever.email@gmail.com'
+email['subject']=  'Email Subject'
 
 email.set_content(html.substitute({'name': 'tintin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login(#email in '' ,# you'r password in '')
+    smtp.login('your.email@gmail.com' ,'YOU"R EMAIL PASSWORD')
     smtp.send_message(email)
     print("all good boss!!")
